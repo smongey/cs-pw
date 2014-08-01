658f462d0699bf77049923d7fd587f7052d2532f
Sean Mongey <sm@smongey.com>
2014-08-01 12:23:08 +0100

    updated gallery iamge sizer

--- templates/gallery.php
+++ templates/gallery.php
@@ -18,7 +18,7 @@
 
 		<div class="item w2">
 			<a href=#>
-			<img src='<?php echo $pic->images->first()->size(600, 400)->url ?>' alt="<?php $pic->main_image->description ?>">
+			<img src='<?php echo $pic->images->size(600)->url ?>' alt="<?php $pic->main_image->description ?>">
 			<h5><?php echo $pic->title ?></h5>
 			</a>
 		</div>
@@ -27,7 +27,7 @@
 
 		<div class="item">
 			<a href=#>
-			<img src='<?php echo $pic->images->first()->width(295)->url ?>' alt="<?php $pic->main_image->description ?>">
+			<img src='<?php echo $pic->images->width(295)->url ?>' alt="<?php $pic->main_image->description ?>">
 			<h5><?php echo $pic->title ?></h5>
 			</a>
 		</div>
